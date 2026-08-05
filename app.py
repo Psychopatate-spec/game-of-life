7f6f487f6ac78055393a6d0f4b17989339f5fccd
Psychopatate-spec <iklassgames@gmail.com>
2026-08-05 19:51:08 +0100

    changed variable names to make everything more readable

--- app.py
+++ app.py
@@ -1,58 +1,61 @@
 import random
+import time
 
-def dead_state(width, height):
-    board_state = [[0 for _ in range(width)] for _ in range(height)]
-    return board_state
+def empty_board(width, height):
+    board = [[0 for _ in range(width)] for _ in range(height)]
+    return board
 
-def random_state(width, height):
-    board_state = [[random.random() for _ in range(width)] for _ in range(height)]
-    for i in range(height):
-        for j in range(width):
-            if board_state[i][j] >= 0.5:
-                board_state[i][j] = 1
+def random_board(width, height):
+    board = [[random.random() for _ in range(width)] for _ in range(height)]
+    for row_index in range(height):
+        for col_index in range(width):
+            if board[row_index][col_index] >= 0.5:
+                board[row_index][col_index] = 1
             else:
-                board_state[i][j] = 0
-    return board_state
+                board[row_index][col_index] = 0
+    return board
 
-def render(board_state):
-    line = ""
-    for i in range(len(board_state)):
-        line = line + "|"
-        for j in range(len(board_state[i])):
-            if board_state[i][j] == 1:
-                line = line + "#"
+def render_board(board):
+    output = ""
+    for row_index in range(len(board)):
+        output += "|"
+        for col_index in range(len(board[row_index])):
+            if board[row_index][col_index] == 1:
+                output += "#"
             else:
-                line = line + " "
-        line = line + "|\n"
-    print(line)
+                output += " "
+        output += "|\n"
+    print(output)
 
-def next_board_state(initial_board_state):
-    height = len(initial_board_state)
-    width = len(initial_board_state[0])
-    next_board = dead_state(width, height)
-    for i in range(height):
-        for j in range(width):
-            neighbor_cells = 0
-            for x in range(-1, 2):
-                for y in range(-1, 2):
-                    if x == 0 and y == 0:
+def compute_next_board(current_board):
+    height = len(current_board)
+    width = len(current_board[0])
+    next_board = empty_board(width, height)
+    for row_index in range(height):
+        for col_index in range(width):
+            live_neighbors = 0
+            for row_offset in range(-1, 2):
+                for col_offset in range(-1, 2):
+                    if row_offset == 0 and col_offset == 0:
                         continue
-                    neighbor_i = i + x
-                    neighbor_j = j + y
-                    if 0 <= neighbor_i < height and 0 <= neighbor_j < width:
-                        if initial_board_state[neighbor_i][neighbor_j] == 1:
-                            neighbor_cells += 1
-            if initial_board_state[i][j] == 1:
-                if neighbor_cells == 2 or neighbor_cells == 3:
-                    next_board[i][j] = 1
+                    neighbor_row = row_index + row_offset
+                    neighbor_col = col_index + col_offset
+                    if 0 <= neighbor_row < height and 0 <= neighbor_col < width:
+                        if current_board[neighbor_row][neighbor_col] == 1:
+                            live_neighbors += 1
+            if current_board[row_index][col_index] == 1:
+                if live_neighbors == 2 or live_neighbors == 3:
+                    next_board[row_index][col_index] = 1
                 else:
-                    next_board[i][j] = 0
+                    next_board[row_index][col_index] = 0
             else:
-                if neighbor_cells == 3:
-                    next_board[i][j] = 1
+                if live_neighbors == 3:
+                    next_board[row_index][col_index] = 1
     return next_board
 
-current_state = random_state(100, 50)
+current_board = random_board(100, 40)
+
 while True:
-    render(current_state)
-    current_state = next_board_state(current_state)
+    render_board(current_board)
+    time.sleep(0.05)
+    current_board = compute_next_board(current_board)
